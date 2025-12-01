27bb294953c6227f6a67d1f9e339d87336ee352c
DS_BDT <daniel.smitton@centralbedfordshire.gov.uk>
2025-12-01 12:50:14 +0000

    Added Status attribute and new instance functions to update ServChange, ServFac, StatusFac and Rag.
    Remove redundant test code

--- 5YearTool.py
+++ 5YearTool.py
@@ -9,27 +9,41 @@ import numpy as np
 dateformat = "%m/%d/%Y"
 
 #create a dataframe of each service variety, intensity level and risk factor modifier
-    #create lists
+#create lists
 ServType = ["Equipment","Day Support","Direct Payment","Homecare:Low","Homecare:Mid","Homecare:High"]
 ServIntens = [1,2,3,4,5,6]
 ServModify = [0.6,0.8,1,1.1,1.3,1.5]
 Header = ["Service","Intensity","RiskModifier"]
-    #combine lists to array
+#combine lists to array
 ServInfo = np.array((ServType))
 ServInfo=ServInfo.reshape(6,1)
 ServInfo = np.hstack((ServInfo,np.array((ServIntens)).reshape(6,1),np.array((ServModify)).reshape(6,1)))
-    #convert array to dataframe
+#convert array to dataframe
 ServInfo = pd.DataFrame(ServInfo,columns = Header)
 ServInfo = ServInfo.set_index("Service")
 
-#Create a new class for individual information
+#create dataframe of key info
+StatusRoute = ["Transition","Community","Hospital Discharge","Existing Service"]
+StatusModify = [1.2,1.2,1.5,1]
+StatusHeader = ["Entry","Modifier"]
+#combine lists to array
+StatusInfo = np.array((StatusRoute)).reshape(4,1)
+StatusInfo = np.hstack((StatusInfo,np.array((StatusModify)).reshape(4,1)))
+#convert to dataframe
+StatusInfo = pd.DataFrame(StatusInfo,columns = StatusHeader).set_index("Entry")
+
+
+#Create a new class for entering person info and generating RAG
 class AssessRev:
-    """This contains details of individuals at the point of a review or assessment that underpin the creation of a non-independent care risk rating
+    """
+    This contains details of individuals at the point of a review or assessment that underpin the creation of a non-independent care risk rating
+
     Methods:
         update_AgeFac() - allows the default AgeFac created at initiation to be replaced with a calculated version
         update_ServFac()
         update_ServChange()
         update_Rag()
+
     Attributes:
         PersonId - This is the unique identification number for the person recieving the assessment or review. Required to be an integer
         ContactDate - The date that the review or assessment was completed
@@ -40,25 +54,31 @@ class AssessRev:
         ServFac - The risk modification factor associated with the service type in NewServ. Default = 1
         ServChange - The risk modification factor associated with the direction of chnage between CurrentServ and NewServ.  Default = 1
         Rag - The combined risk score of the person entering non-independent care within the next 12 months, Default = 1
-    """
-    #add validation list for service types (CurrentServ and NewServ)
-    ServType = ServType
+    """  
+    #add validation lists for service type and status
+    SERV_TYPES = ServType
+    STATUS_TYPES = StatusRoute
 
     #create initialisation method to include key details at initialisation
-    def __init__(self,PersonId,ContactDate,BirthDate,CurrentServ,NewServ,AgeFac=1,ServFac=1,ServChange=1,Rag=1):
+    def __init__(self,PersonId,ContactDate,BirthDate,Status,CurrentServ,NewServ,AgeFac=1,ServFac=1,ServChange=1,StatusFac=1,Rag=1):
         self.PersonId = PersonId
         self.ContactDate = pd.to_datetime(ContactDate,dayfirst=True)
         self.BirthDate = pd.to_datetime(BirthDate,dayfirst=True)
+        self.Status = Status
         self.CurrentServ = CurrentServ
         self.NewServ = NewServ
         self.AgeFac = AgeFac
         self.ServFac = ServFac
         self.ServChange=ServChange
+        self.StatusFac = StatusFac
         self.Rag = Rag
 
     #create method to update AgeFac
     def update_AgeFac(self):
-        """This method allows the replacement of the default AgeFac with a calculated value based on ageband"""
+        """
+        This method allows the replacement of the default AgeFac with a calculated value based on ageband
+        No additional attributes are required
+        """
         dob = pd.to_datetime(self.BirthDate)
         doc = pd.to_datetime(self.ContactDate)
         ageCalc = (doc-dob).days
@@ -75,81 +95,56 @@ class AssessRev:
     
     #create method to update ServFac
     def update_ServFac(self):
-        """This method allows the replacement of the existing ServFac of an AssessRev instance with a new value"""
+        """
+        This method allows the replacement of the existing ServFac of an AssessRev instance with a new value
+        No additional attributes are required
+        """
         newServFac = ServInfo.at[self.NewServ,"RiskModifier"]
         self.ServFac = newServFac
 
     #create method to update ServChange
-    def update_ServChange(self,ServChangeNew):
-        """This method allows the replacement of the existing ServChange of an AssessRev instance with a new value"""
-        self.ServChange = ServChangeNew
+    def update_ServChange(self):
+        """
+        This method allows the replacement of the existing ServChange of an AssessRev instance with a new value
+        No additional attributes are required
+        """
+        currVal = ServInfo.at[self.CurrentServ,"Intensity"]
+        newVal = ServInfo.at[self.NewServ,"Intensity"]
+        change = int(newVal)-int(currVal)
+        if change > 2:
+            changeFac = 2
+        elif change > 1:
+            changeFac = 1.75
+        elif change > 0:
+            changeFac = 1.5
+        elif change == 0:
+            changeFac = 1
+        elif change < -2:
+            changeFac = 0.25
+        elif change < -1:
+            changeFac = 0.5
+        else:
+            changeFac = 0.75
+        self.ServChange = changeFac
 
-    #create method to update RAG
-    def update_Rag(self,RagNew):
-        """This method allows the replacement of the existing RAG of an AssessRev instance with a new value"""
-        self.Rag=RagNew
-      
+    #add a new function to update the modification factor associated with entry route
+    def update_StatusFac(self):
+        StatusMod = StatusInfo.at[self.Status,"Modifier"]
+        self.StatusFac = StatusMod
+
+    #add function to recalculate RAG based on risk factors
+    def update_Rag(self):
+        newRag = float(self.Rag)*float(self.ServFac)*float(self.AgeFac)*float(self.ServChange)*float(self.StatusFac)
+        self.Rag = newRag
+    #add class methods to restrict values for NewServ,CurrServ and Status
+
     pass
 
-assess1 = AssessRev("789231","21/05/2025","13/07/1935","Homecare:Low","Homecare:Mid")
-
-#funtion to assign a modifier based on direction and magnitude of service intensity change before including in class as an instance function
-currVal = ServInfo.at[assess1.CurrentServ,"Intensity"]
-newVal = ServInfo.at[assess1.NewServ,"Intensity"]
-change = int(newVal)-int(currVal)
-if change > 2:
-    changeFac = 2
-elif change > 1:
-    changeFac = 1.75
-elif change > 0:
-    changeFac = 1.5
-elif change == 0:
-    changeFac = 1
-elif change < -2:
-    changeFac = 0.25
-elif change < -1:
-    changeFac = 0.5
-else:
-    changeFac = 0.75
-
-
-print(changeFac)
-
-#assess1.update_ServFac()
-#print(assess1.ServFac)
-
-#### remove code below here (testing area)
-#assess1.update_AgeFac(1.5)
-#print(assess1.AgeFac)
-
-# create function to recalculate and replace the AgeFac of a AssessRev Instance
-#def calc_AgeFactor(ID):
-  #  dob = pd.to_datetime(ID.BirthDate)
- #   doc = pd.to_datetime(ID.ContactDate)
- #   AgeCalc =  (doc-dob).days
-  #  AgeCalc = math.floor(AgeCalc/365)
-  #  if AgeCalc >85:
-   #     AgeCalc = 1.5
-  ##  elif AgeCalc >75:
-   #     AgeCalc = 1.2
-   # else:
-  #      AgeCalc = 1
-   # ID.update_AgeFac(AgeCalc)
-
-#calc_AgeFactor(assess1)
-#print(assess1.AgeFac)
-#assess1.update_AgeFac()
-#print(assess1.AgeFac)
-
-
-#arr1 = np.array(["Person_ID","BirthDate","ContactDate","CurrentServ","NewServ","RAG"])
-#list = ["yes","No","Why","Contact","in","Shoes"]
-#arr2=np.array(list)
-#arr3 = np.stack((arr1,arr2))
-#df = pd.DataFrame(arr3)
-#arr3 = np.vstack((arr3,arr2))
-#arr4 = np.vstack((Contact_Array,arr2))
-
-#new = create_Contact(assess1)
-
-#create a matrix to store service information (ServFac,Intensity Score)
+assess1 = AssessRev("789231","21/05/2025","13/07/1935","Hospital Discharge","Homecare:Low","Homecare:Mid")
+assess1.update_ServChange()
+assess1.update_AgeFac()
+assess1.update_ServFac()
+assess1.update_StatusFac()
+assess1.update_Rag()
+#print("Rag:",assess1.Rag,"ServFac:",assess1.ServFac,"ServChange:",assess1.ServChange,"AgeFac:",assess1.AgeFac,"StatusFac:",assess1.StatusFac)
+print(ServInfo)
